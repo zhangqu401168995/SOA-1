6169635532a0c7bacb58b37acc53090870a17fcc
zhangqu401168995 <401168995@qq.com>
2020-08-17 16:01:04 +0800

    新加入用户管理模块

---
+++ soa-user-manage/src/test/java/com/zhangqu/soa/soausermanage/SoaUserManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangqu.soa.soausermanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaUserManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
